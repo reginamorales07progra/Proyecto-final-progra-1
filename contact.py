cae2919069d631d389661c2e46afded670d727d7
Regina Morales <reginamorales@MacBook-Pro-de-Mac.local>
2020-05-12 13:53:05 -0600

    contacto2

--- contact.py
+++ contact.py
@@ -34,3 +34,55 @@ def validate(contact):
         fail["telefono"] = contact["telefono"]
 
     return fail
+
+    def crearContacto():
+
+    print("Ingrese el contacto\n")
+    contact = {
+        "nombre": input("nombre: ").lower(),
+        "telefono": input("telefono: "),
+        "correo": input("correo: "),
+        "empresa": input("empresa: "),
+        "extra": input("extra: ")
+
+    }
+    fail = validate(contact)
+    clear()
+    print("\nverificando...")
+
+    while len(fail.keys()):
+        for key in fail.keys():
+            data = input(f'ingresaste mal el {key}, intentalo de nuevo: ')
+            contact[key] = data
+        fail = validate(contact)
+
+    char = contact["nombre"][:1].upper()
+    contacts[char][contact["nombre"]] = contact
+
+    clear()
+    print("agregado exitosamente\n")
+
+
+def showPerson(person):
+    for name, data in person.items():
+        print(f'{name}: ')
+        for key, value in data.items():
+            print(f'\t{key}: {value}')
+
+
+def listarContactos():
+    letters = contacts.keys()
+    cts = []
+    count = 1
+    for letter in letters:
+        persons = contacts[letter]
+        print(letter)
+        for name, data in persons.items():
+            print(f'   {count}. {name}')
+            count += 1
+            cts.append({name: data})
+
+    select = input("Ver contacto: ").lower()
+    if len(select):
+        person = cts[int(select) - 1]
+        showPerson(person)
